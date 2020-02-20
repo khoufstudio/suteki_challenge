66822964c409408b750cf15840a08c30c1fce614
Rizqy Nurhaqy <rizqy.nurhaqy@gmail.com>
2020-02-20 13:59:29 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,43 @@
 </template>
 
 <script>
+/*eslint no-mixed-spaces-and-tabs: ["error", "smart-tabs"]*/
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      baseUrl: process.env.VUE_APP_BASE_URL,
+    }
+  },
+  methods: {
+    fetchData() {
+      axios.get(this.baseUrl + 'employee.json')
+        .then(response => {
+          console.log(response.data)
+        })
+    }
+  },
+  created() {
+    // console.log(this.baseUrl)
+    this.fetchData()
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  $color: blue;
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: $color;
+    margin-top: 60px;
+  }
 </style>
