4970bf580c141ffd5123d4d7ddd41d9d365478f7
Rizqy Nurhaqy <rizqy.nurhaqy@gmail.com>
2020-02-20 14:18:58 +0700

    ganti menggunakan tab (4 spasi)

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<img alt="Vue logo" src="./assets/logo.png">
+		<HelloWorld msg="Welcome to Your Vue.js App"/>
+	</div>
 </template>
 
 <script>
-/*eslint no-mixed-spaces-and-tabs: ["error", "smart-tabs"]*/
 import HelloWorld from './components/HelloWorld.vue'
 import axios from 'axios'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      baseUrl: process.env.VUE_APP_BASE_URL,
-    }
-  },
-  methods: {
-    fetchData() {
-      axios.get(this.baseUrl + 'employee.json')
-        .then(response => {
-          console.log(response.data)
-        })
-    }
-  },
-  created() {
-    // console.log(this.baseUrl)
-    this.fetchData()
-  }
+	name: 'App',
+	components: {
+		HelloWorld
+	},
+	data() {
+		return {
+			baseUrl: process.env.VUE_APP_BASE_URL,
+		}
+	},
+	methods: {
+		fetchData() {
+			axios.get(this.baseUrl + 'employee.json')
+				.then(response => {
+			  		console.log(response.data)
+				})
+		}
+	},
+	created() {
+		this.fetchData()
+	}
 }
 </script>
 
 <style lang="scss">
-  $color: blue;
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: $color;
-    margin-top: 60px;
-  }
+	$color: blue;
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: $color;
+		margin-top: 60px;
+	}
 </style>
